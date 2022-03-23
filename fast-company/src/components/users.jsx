c73357fd4c068d9f7a71dbca1b7923387c74b398
medkov77 <medkov77@mail.rum>
2022-03-23 20:54:38 +0300

    done

--- fast-company/src/components/users.jsx
+++ fast-company/src/components/users.jsx
@@ -1,49 +1,95 @@
-import React, {  useState } from "react";
-import api from "../api"
+import React, { useState } from "react";
+import api from "../api";
 const Users = () => {
-  const usersDataSource = api.users.fetchAll();
-  console.log(usersDataSource[0]);
-  const [usersData, setUserData] = useState (usersDataSource);
+  function renderPhrase(n) {
+    if (n < 2 || (n >= 5 && n <= 21)) {
+      return "человек";
+    }
+    if (n > 1 && n < 5) {
+      return "человека";
+    }
+    let nLast = Number(String(n).slice(-1));
+    if (nLast > 1 && nLast < 5) {
+      return "человека";
+    }
 
+    return "человек";
+  }
+
+  const usersDataSource = api.users.fetchAll();
+  const [usersData, setUserData] = useState(usersDataSource);
+  const [counter, setCounter] = useState(usersData.length);
+  const [phrase, setPhrase] = useState(renderPhrase(counter));
   function handleDelite(id) {
-    setUserData((prevState)=>prevState.filter(user=>user._id !== id))
+    setUserData((prevState) => prevState.filter((user) => user._id !== id));
+    setCounter(counter - 1);
+    setPhrase(renderPhrase(counter - 1));
   }
 
   return (
-  <div className="container">
-  <table className="table">
-  <thead className ="mb-2">
-    <tr>
-      <th scope="col">Имя</th>
-      <th scope="col">Качества</th>
-      <th scope="col">Профессия</th>
-      <th scope="col">Встретился, раз</th>
-      <th scope="col">Оценка</th>
-      <th scope="col"></th>
-    </tr>
-  </thead>
-  <tbody className = "mt-2 pt-2">
-    
-     {usersData.map((user)=>{
-      return(
-      <tr scope="col" key = {user._id} className="align-middle">
-        <td >{user.name}</td>
-        <td >{user.qualities.map((qualiti)=>{ return (
-          <span className= {`m-2 p-2 badge bg-${qualiti.color}`} key = {qualiti._id}>{qualiti.name}</span>) })}</td>
-        <td>{user.profession.name}</td>    
-        <td>{user.completedMeetings}</td>    
-        <td>{user.rate}/5</td>    
-        <td><button type="button" 
-              className="btn btn-danger bg-red"
-              onClick = {()=> handleDelite(user._id)}>delite</button></td>
-      </tr>)
-      
-     })} 
-      
-  </tbody>
-</table>
-</div>
-  )
+    <div className="container">
+      {counter !== 0 ? (
+        <div className="container">
+          <h3>
+            <span className="mt-2 badge bg-primary">
+              {counter} {phrase} тусанет с тобой сегодня{" "}
+            </span>
+          </h3>
+          <table className="table">
+            <thead className="mb-2">
+              <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Качества</th>
+                <th scope="col">Профессия</th>
+                <th scope="col">Встретился, раз</th>
+                <th scope="col">Оценка</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody className="mt-2 pt-2">
+              {usersData.map((user) => {
+                return (
+                  <tr scope="col" key={user._id} className="align-middle">
+                    <td>{user.name}</td>
+                    <td>
+                      {user.qualities.map((qualiti) => {
+                        return (
+                          <span
+                            className={`m-2 p-2 badge bg-${qualiti.color}`}
+                            key={qualiti._id}
+                          >
+                            {qualiti.name}
+                          </span>
+                        );
+                      })}
+                    </td>
+                    <td>{user.profession.name}</td>
+                    <td>{user.completedMeetings}</td>
+                    <td>{user.rate}/5</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-danger bg-red"
+                        onClick={() => handleDelite(user._id)}
+                      >
+                        delite
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      ) : (
+        <h3>
+          <span className="mt-2 badge bg-danger">
+            никто не тусанет с тобой сегодня
+          </span>
+        </h3>
+      )}
+    </div>
+  );
 };
 
 export default Users;
