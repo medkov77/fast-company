dca5a304eb72d7d4fb968ce477f3584ff6d28978
medkov77 <medkov77@mail.ru>
2022-03-23 16:40:01 +0300

    begin

--- fast-company/src/components/users.jsx
+++ fast-company/src/components/users.jsx
@@ -1,7 +1,41 @@
 import React from "react";
-
+import api from "../api"
 const Users = () => {
-  return <h1>Users</h1>;
+ const usersData = api.users.fetchAll();
+  console.log(usersData[0]);
+  return (
+  <div className="container">
+  <table className="table">
+  <thead className ="mb-2">
+    <tr>
+      <th scope="col">Имя</th>
+      <th scope="col">Качества</th>
+      <th scope="col">Профессия</th>
+      <th scope="col">Встретился, раз</th>
+      <th scope="col">Оценка</th>
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody className = "mt-2 pt-2">
+    
+     {usersData.map((user)=>{
+      return(
+      <tr scope="col" key = {user._id} className="align-middle">
+        <td >{user.name}</td>
+        <td >{user.qualities.map((qualiti)=>{ return (
+          <span className= {`m-2 p-2 badge bg-${qualiti.color}`} key = {qualiti._id}>{qualiti.name}</span>) })}</td>
+        <td>{user.profession.name}</td>    
+        <td>{user.completedMeetings}</td>    
+        <td>{user.rate}/5</td>    
+        <td><button type="button" className="btn btn-danger bg-red">delite</button></td>
+      </tr>)
+      
+     })} 
+      
+  </tbody>
+</table>
+</div>
+  )
 };
 
 export default Users;
