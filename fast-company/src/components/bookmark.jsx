ea97c3585a18b7b74107efbb896f7410df1858a7
medkov77 <medkov77@mail.ru>
2022-03-31 15:28:52 +0300

    add color too bookmark

--- fast-company/src/components/bookmark.jsx
+++ fast-company/src/components/bookmark.jsx
@@ -7,7 +7,7 @@ const BookMark = (props) => {
   return (
     <td
     onClick = { ()=> props.onToggleBookMark(props._id)}>
-     {props.bookmark? <i className="bi bi-bookmark-fill"  ></i> : <i className="bi bi-bookmark"></i>}
+     {props.bookmark? <i class="bi bi-arrow-through-heart-fill"/> : <i class="bi bi-arrow-through-heart"/>}
     </td>
   )
 }
