5b697cdcc5ac4f32a863622d880e425a3c5e409e
medkov77 <medkov77@mail.rum>
2022-04-12 19:05:16 +0300

    old api

--- fast-company/src/components/bookmark.jsx
+++ fast-company/src/components/bookmark.jsx
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 const BookMark = ({ bookmark, onToggleBookMark, _id }) => {
     return (
         <td onClick={() => onToggleBookMark(_id)}>
-            { bookmark
-                ? <i className="bi bi-arrow-through-heart-fill" />
-                : <i className="bi bi-arrow-through-heart" />
-            }
+            {bookmark ? (
+                <i className="bi bi-arrow-through-heart-fill" />
+            ) : (
+                <i className="bi bi-arrow-through-heart" />
+            )}
         </td>
     );
 };
-
 BookMark.propTypes = {
     bookmark: PropTypes.bool.isRequired,
     _id: PropTypes.number.isRequired,
