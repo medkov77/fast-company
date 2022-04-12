98a7095a56309c19c826808e34646fa708ad24d3
medkov77 <medkov77@mail.ru>
2022-04-12 17:08:07 +0300

    new api

---
+++ fast-company/src/app/components/users.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { paginate } from "../utils/paginate";
+import Pagination from "./pagination";
+import User from "./user";
+import GroupList from "./groupList";
+import api from "../api";
+const Users = ({ users: allUsers, ...rest }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions] = useState(api.professions.fetchAll());
+    const count = allUsers.length;
+    const pageSize = 4;
+
+    const ProfessionSelect = (params) => {
+        console.log(params);
+    };
+    console.log(professions);
+    const handlePageChange = (pageIndex) => {
+        setCurrentPage(pageIndex);
+        console.log("page: ", pageIndex);
+    };
+    const usersCrop = paginate(allUsers, currentPage, pageSize);
+    return (
+        <>
+            <GroupList items={professions} onItemSelect={ProfessionSelect} />
+            {count > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Имя</th>
+                            <th scope="col">Качества</th>
+                            <th scope="col">Провфессия</th>
+                            <th scope="col">Встретился, раз</th>
+                            <th scope="col">Оценка</th>
+                            <th scope="col">Избранное</th>
+                            <th />
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {usersCrop.map((user) => (
+                            <User {...rest} {...user} key={user._id} />
+                        ))}
+                    </tbody>
+                </table>
+            )}
+            <Pagination
+                itemsCount={count}
+                pageSize={pageSize}
+                currentPage={currentPage}
+                onPageChange={handlePageChange}
+            />
+        </>
+    );
+};
+Users.propTypes = {
+    users: PropTypes.array
+};
+
+export default Users;
