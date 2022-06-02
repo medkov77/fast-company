3f49e0590dfba173ba35026c4aa3bdaddf65eafa
medkov77 <medkov77@mail.rum>
2022-06-02 19:17:26 +0300

    qualities start

--- fast-company/src/app/components/common/form/selectField.jsx
+++ fast-company/src/app/components/common/form/selectField.jsx
@@ -21,7 +21,7 @@ const SelectField = ({
         !Array.isArray(options) && typeof options === "object"
             ? Object.values(options)
             : options;
-    console.log(options);
+
     return (
         <div className="mb-4">
             <label htmlFor={name} className="form-label">
